a4d9a76f14687a8f483f93817795ab83fdaece27
sang5c <skah321@gmail.com>
2023-08-30 10:45:06 +0900

    refactor(MenuGroupServiceTest): fake로 대체

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -2,37 +2,34 @@ package kitchenpos.application;
 
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.fakeobject.InMemoryMenuGroupRepository;
 import kitchenpos.fixture.MenuGroupFixtures;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
 class MenuGroupServiceTest {
 
-    @Mock
     private MenuGroupRepository menuGroupRepository;
-
-    @InjectMocks
     private MenuGroupService menuGroupService;
+
+    @BeforeEach
+    void setup() {
+        menuGroupRepository = new InMemoryMenuGroupRepository();
+        menuGroupService = new MenuGroupService(menuGroupRepository);
+    }
 
     @DisplayName("메뉴 그룹을 등록한다")
     @Test
     void create() {
         MenuGroup menuGroup = MenuGroupFixtures.createMenuGroup("그룹1");
-        given(menuGroupRepository.save(any())).willReturn(menuGroup);
 
         MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
 
@@ -53,12 +50,11 @@ class MenuGroupServiceTest {
     @DisplayName("메뉴 그룹 목록을 조회한다")
     @Test
     void findAll() {
-        MenuGroup menuGroup1 = MenuGroupFixtures.createMenuGroup("그룹1");
-        MenuGroup menuGroup2 = MenuGroupFixtures.createMenuGroup("그룹2");
-        given(menuGroupRepository.findAll()).willReturn(List.of(menuGroup1, menuGroup2));
+        menuGroupRepository.save(MenuGroupFixtures.createMenuGroup("그룹1"));
+        menuGroupRepository.save(MenuGroupFixtures.createMenuGroup("그룹2"));
 
         List<MenuGroup> groups = menuGroupService.findAll();
-        
-        assertThat(groups).containsExactly(menuGroup1, menuGroup2);
+
+        assertThat(groups).hasSize(2);
     }
 }
